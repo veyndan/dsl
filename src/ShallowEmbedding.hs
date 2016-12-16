2678fe29b4e8b1a9c9b806eabef60419329fcfda
veyndan <veyndan89@gmail.com>
2016-12-16 18:50:52 +0000

    Remove references to background

--- src/ShallowEmbedding.hs
+++ src/ShallowEmbedding.hs
@@ -4,24 +4,23 @@ test = do
           print (design == ((50, 65), True, ["Hello world", "It's Veyndan", "Wazaaaa", "The show has finished"]))
 
 design :: View
-design = linearLayout Vertical (0, 255, 0, 0.5) [
-             textView (20, 30) (255, 0, 0, 0.3) "Hello world",
-             textView (40, 15) (0, 0, 255, 1) "It's Veyndan",
-             linearLayout Horizontal (127, 127, 0, 0.8) [
-                 textView (10, 20) (0, 127, 127, 0.4) "Wazaaaa",
-                 textView (40, 15) (0, 127, 0, 1) "The show has finished"
+design = linearLayout Vertical [
+             textView (20, 30) "Hello world",
+             textView (40, 15) "It's Veyndan",
+             linearLayout Horizontal [
+                 textView (10, 20) "Wazaaaa",
+                 textView (40, 15) "The show has finished"
              ]
          ]
 
 type Size = (Int, Int)
 data Orientation = Horizontal | Vertical
-type Background = (Int, Int, Int, Float)
 
 type View = (Size, Bool, [String])
 
-linearLayout :: Orientation -> Background -> [View] -> View
-linearLayout Vertical _ vs = ((maximum [w | ((w, _), _, _) <- vs], sum [h | ((_, h), _, _) <- vs]), and [v | (_, v, _) <- vs], concat [t | (_, _, t) <- vs])
-linearLayout Horizontal _ vs = ((sum [w | ((w, _), _, _) <- vs], maximum [h | ((_, h), _, _) <- vs]), and [v | (_, v, _) <- vs], concat [t | (_, _, t) <- vs])
+linearLayout :: Orientation -> [View] -> View
+linearLayout Vertical vs = ((maximum [w | ((w, _), _, _) <- vs], sum [h | ((_, h), _, _) <- vs]), and [v | (_, v, _) <- vs], concat [t | (_, _, t) <- vs])
+linearLayout Horizontal vs = ((sum [w | ((w, _), _, _) <- vs], maximum [h | ((_, h), _, _) <- vs]), and [v | (_, v, _) <- vs], concat [t | (_, _, t) <- vs])
 
-textView :: Size -> Background -> String -> View
-textView s@(w, h) _ t = (s, w >= 0 && h >= 0, [t])
+textView :: Size -> String -> View
+textView s@(w, h) t = (s, w >= 0 && h >= 0, [t])
